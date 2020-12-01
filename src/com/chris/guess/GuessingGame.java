d825cbfd8fae973d4478590be9aa231750e0563a
Cjohnson187 <chrisfjohnson3@gmail.com>
2020-12-01 13:40:32 -0700

    added resource bundle function for internationalization.

--- src/com/chris/guess/GuessingGame.java
+++ src/com/chris/guess/GuessingGame.java
@@ -1,7 +1,7 @@
 package com.chris.guess;
 
 
-import java.util.Scanner;
+import java.util.*;
 
 
 class GuessingGame {
@@ -13,6 +13,25 @@ class GuessingGame {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		//Found on java docs Internationalization example
+		String language;
+		String country;
+		if (args.length != 2) {
+			language = new String("en");
+			country = new String("US");
+		} else {
+			language = new String(args[0]);
+			country =  new String(args[1]);
+		}
+		
+		Locale currentLocale;
+		ResourceBundle messages;
+		
+		currentLocale = new Locale(language, country);
+		messages = ResourceBundle.getBundle("com.chris.guess.MessagesBundle", currentLocale);
+		System.out.println(messages.getString("final1"));
+		
 		int currentGameNum = 0;
 		Scanner input = new Scanner(System.in);
 		String[] basicInfo = getBasicInfo(input);
